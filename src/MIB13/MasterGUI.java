182d9a5edeb61970e1dd7ed3500a75acbdff0a1f
Alex <demirioni@gmail.com>
2014-10-26 00:04:58 +0200

    weitere anpassung am timelabel

--- src/MIB13/MasterGUI.java
+++ src/MIB13/MasterGUI.java
@@ -132,7 +132,7 @@ public class MasterGUI {
         }
 
         //Label für Zeit
-        timeLabel = new JLabel("Zeit: ");
+        timeLabel = new JLabel("Zeit: 0");
         timeLabel.setSize(100, 20);
         timeLabel.setForeground(Color.white);
         controlPanel.add(timeLabel);
