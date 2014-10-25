d2b2a619d12026b52af3fc36489d62f4c59b6153
Alex <demirioni@gmail.com>
2014-10-26 00:06:46 +0200

    meint testreiber -> achtung!!! für richtige anzeige des couters bei euren die 2 zeilen einfügen: Counter mytimer = new Counter();     	Counter.timer.stop();

---
+++ src/MIB13/TestDriverAlex.java
@@ -0,0 +1,9 @@
+package MIB13;
+
+public class TestDriverAlex {
+	public static void main(String[] args) {
+        MasterGUI gui = new MasterGUI();
+		Counter mytimer = new Counter();
+    	Counter.timer.stop();
+    }
+}
