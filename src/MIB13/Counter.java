83bb08e2bb20de4a110ecb2612f27f6275fcfa08
Alex <demirioni@gmail.com>
2014-10-26 00:04:09 +0200

    Counter hotfix

--- src/MIB13/Counter.java
+++ src/MIB13/Counter.java
@@ -8,13 +8,13 @@ import java.awt.event.*;
 
 public class Counter extends JFrame {
 
-	private Timer timer;
+	public static Timer timer;
 	public static int count = 0;
 
 	//Timer constructor
 	public Counter() {
 		timer = new Timer(1000, new TimerListener()); //Tempo
-		timer.start();
+		//timer.start();
 	}
 
 	private class TimerListener implements ActionListener {
