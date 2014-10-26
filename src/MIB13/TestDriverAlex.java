f2ac5f7ed11906797a2d34b16b241d189227b254
Alex <demirioni@gmail.com>
2014-10-26 12:22:04 +0100

    test

--- src/MIB13/TestDriverAlex.java
+++ src/MIB13/TestDriverAlex.java
@@ -3,7 +3,5 @@ package MIB13;
 public class TestDriverAlex {
 	public static void main(String[] args) {
         MasterGUI gui = new MasterGUI();
-		Counter mytimer = new Counter();
-    	Counter.timer.stop();
     }
 }
